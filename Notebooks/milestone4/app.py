4ede47b42b6b0f703e7e74c3c4757d57613982d3
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-04-21 11:10:59 -0700

    Update app.py

--- Notebooks/milestone4/app.py
+++ Notebooks/milestone4/app.py
@@ -10,13 +10,10 @@ app = Flask(__name__)
 model = joblib.load("model.joblib")
 
 # 2. Define a prediction function
-def return_prediction(input):
+def return_prediction(data):
 
     # format input_data here so that you can pass it to model.predict()
-    X = input["data"]
-    X = np.array(X)
-    X = X.reshape(1, -1)
-    return model.predict(X)
+    return model.predict([data])
 
 
 # 3. Set up home page using basic html
@@ -33,9 +30,10 @@ def index():
 @app.route("/predict", methods=["POST"])
 def rainfall_prediction():
     content = request.json  # this extracts the JSON content we sent
-    prediction = return_prediction(content)
+    prediction = return_prediction(content["data"])
     results = {
-        "prediction": dict(enumerate(prediction.flatten(), 1)),
+        # "Input": content["data"],
+        "prediction": prediction[0],
     }  # return whatever data you wish, it can be just the prediction
     # or it can be the prediction plus the input data, it's up to you
     return jsonify(results)
